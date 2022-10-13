74ee95668db6d34925a0eddd13819169b361e7ae
wming <wming@hnu.edu.cn>
2022-10-13 21:10:22 +0800

    TEST

--- test.cpp
+++ test.cpp
@@ -1,33 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-void
-print_result(FILE *fp)
-{
-        char buf[100];
- 
-        if(!fp) {
-                return;
-        }
-        printf("\n>>>\n");
-        while(memset(buf, 0, sizeof(buf)), fgets(buf, sizeof(buf) - 1, fp) != 0 ) {
-                printf("%s", buf);
-        }
-        printf("\n<<<\n");
-}
+using namespace cv;
+using namespace std;
 
-int
-main(void)
-{
-        FILE *fp = NULL;
-        fp = NULL;
-        fp = popen("cd pythonmicroserver-update_weights && python test.py", "r");
-        if(!fp) {
-                perror("popen");
-                exit(EXIT_FAILURE);
-        }
-        print_result(fp);
-        pclose(fp);
+int main() {
+    Mat src = imread("/data/cwm/Graduation/photos/1.png");
+    
+    return 0;
 }
